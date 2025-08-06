d2aa07f84677e68390b422f15862189f06e88a0f
Nevil Biju <nevilbiju9497@gmail.com>
2025-08-07 00:58:15 +0530

    cm01

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -66,7 +66,7 @@ export const Home = () => {
           id="projects"
           className="min-h-screen flex items-center justify-center p-8 text-white"
         >
-          <h2 className="text-3xl">Projects</h2>
+          <h2 className="text-3xl">Projects :</h2>
         </section>
 
         {/* Add more sections as needed */}
