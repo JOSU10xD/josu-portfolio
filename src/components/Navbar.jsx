4107412885c0dba6b1b4c9355745b5680087a485
JOSU10xD <nevilbiju9497@gmail.com>
2026-08-30 23:15:14 +0530

    exp-element-change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import { cn } from "../lib/utils";
 const navItems = [
   { name: "Home", href: "#home" },
   { name: "About", href: "#about" },
-  { name: "Experience", href: "#experience" },
+  { name: "Experience", href: "#projects" },
   { name: "Projects", href: "#projects" },
   { name: "Contact", href: "#contact" },
 ];
